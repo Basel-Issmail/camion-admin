94962de6732e9ad12b920f1b2e82b54de05646ae
Basel Issmail <basel.issmail@gmail.com>
2019-01-26 08:19:37 +0200

    create dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LoginComponent,
-  }
-
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard] }
 ];
 
 @NgModule({
