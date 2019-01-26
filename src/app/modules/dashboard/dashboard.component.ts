442aacf496ff9948385b8e4deb2db81dc5049c5b
Basel Issmail <basel.issmail@gmail.com>
2019-01-26 09:26:09 +0200

    color tabs

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -7,10 +7,20 @@ import { SpinnerService } from 'src/app/shared/services/spinner.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  currentIndex = 0;
 
   constructor(public spinnerService: SpinnerService) { }
 
   ngOnInit() {
+  }
+
+  tabChange($event) {
+    const colors = ['#1e88e5', '#e07000', '#871F78', '#757575', '#d93025', '#3F704D', '#fff00f'];
+    this.currentIndex = $event.index;
+
+    // @ts-ignore
+    document.querySelector('mat-ink-bar').style.backgroundColor = colors[this.currentIndex];
+
   }
 
 }
