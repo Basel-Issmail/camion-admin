be2c06963c45c966c9e3283d3774c122e5df662d
Basel Issmail <basel.issmail@gmail.com>
2019-02-21 06:53:26 +0200

    add translate file

---
+++ src/config/translate.ts
@@ -0,0 +1,21 @@
+export const itianLanguage = {
+    // Language
+    english: 'Inglese',
+    italian: 'Italiano',
+
+    // Services
+    dtco: 'Attivazione e revisione creonotachigrafi digitali DTCO',
+    generatorReplacementRepair: 'Riparazione/montaggio Generatori',
+    mctc: 'Preparazione e revisione MCTC',
+    mobileWorkshop: 'Officina mobile',
+    semiTrailerRepair: 'Riparazione semirimorchi',
+    tarpaulinReplacementRepair: 'Riparazione/sostituzione teloni',
+    tireRepair: 'Gommista',
+    towingCranes: 'Traino/gru',
+    vehicleWash: 'Lavaggio interno/esterno veicoli',
+    windshieldRepair: 'Riparazione parabrezza',
+    rescue: 'Soccorso',
+
+    ALLOW_HIGHWAY: 'Autorizzato autostrada',
+    NO_HIGHWAY: 'NON Autorizzato autostrada'
+};
